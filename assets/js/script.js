280a0f6164f00c0ef3eb3cedaa037a38f3726078
Jake Toomey <jtoom238@gmail.com>
2021-11-06 16:19:46 -0400

    image url functionality

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,8 @@
-var image_url = "https://static.wikia.nocookie.net/topstrongest/images/3/3b/Street_fighter_iii_3rd_strike_alex_by_hes6789_d8zgi1w-fullview.png/revision/latest?cb=20200502115624";
-var url = `https://api.imagga.com/v2/tags?image_url=${image_url}`; //imagga api template literal url, needs to take from input field later
+var image_url = null;
+var url = 'https://api.imagga.com/v2/tags?image_url='+image_url; //imagga api template literal url, needs to take from input field later
 var clientId = "acc_7fb17317fba6ba0";//api key/ USER
-var clientSecret = "f56b7dbf6684c3bf156d23228e990b8f";//api_secret/ PASS
-var searchTerm = document.querySelector('#searchTerm').value;
+var searchInput = null;
+var searchTerm = "";
 
 var myHeaders = new Headers();
 myHeaders.append("Authorization", "Basic YWNjXzdmYjE3MzE3ZmJhNmJhMDpmNTZiN2RiZjY2ODRjM2JmMTU2ZDIzMjI4ZTk5MGI4Zg==");
@@ -29,19 +29,21 @@ function randomNoRepeats(array) {
   };
 }
 
-// commented out Imagga call, gives CORS error/ if no-cors, 401. Opaque data useless anyways
-
+// Imagga call via url
+function urlFunction(){
   fetch(url,requestOptions)
     .then(function(response) {
       return response.json();
     })
-    .then(function(data) {
-      console.log(data);
+    .then(function(response) {
+      searchTerm = response.result.tags[0];
     });
+    metFunction();
+  }
 
 
     // met api call
-    function myFunction() {
+    function metFunction() {
       // Make a fetch request to search with user input
         fetch(
           'https://collectionapi.metmuseum.org/public/collection/v1/search?q=' +
@@ -76,10 +78,27 @@ function randomNoRepeats(array) {
             responseContainerEl.appendChild(metImg);
           }
         });
-          // Return a fetch request to the Metropolitan API with the object data array from the search
         }, 4);
         })
-        
-        
+    };
+    function keywordFunction(){
+      if (searchInput != ""){
+        document.getElementById('imageURL').value = ''
+        searchTerm = searchInput;
+        var responseContainerEl = document.querySelector('#response-container');
+        responseContainerEl.innerHTML = '';
+        searchInput = document.querySelector('#searchTerm').value;
+        metFunction();
+      }
     }
+
+    function imguFunction(){
+      if (image_url != ""){
+        document.getElementById('searchTerm').value = ''
+        var responseContainerEl = document.querySelector('#response-container');
+        responseContainerEl.innerHTML = '';
+        image_url = document.querySelector('#imageURL').value;
+        urlFunction();
+      }
+      };
     
